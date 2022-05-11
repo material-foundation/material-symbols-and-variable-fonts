0f8e190b1f8d38f8832f778b3f28c26cc7f30884
Rody Davis <rody.davis.jr@gmail.com>
2022-05-11 10:59:04 +0200

    Update code-output.ts

--- src/components/code-output.ts
+++ src/components/code-output.ts
@@ -23,8 +23,8 @@ import { unsafeHTML } from "lit/directives/unsafe-html.js";
 export class CodeOutput extends LitElement {
   static styles = css`
     .code {
-      color: var(--md-sys-color-on-surface-variant);
-      background-color: var(--md-sys-color-surface-variant);
+      color: var(--md-sys-color-on-surface);
+      background-color: var(--md-sys-color-surface);
       width: 100%;
       height: 100%;
       display: flex;
